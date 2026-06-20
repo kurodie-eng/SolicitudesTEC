3946f3244c827a8d8b5f18db6628e5a867f7e5cb
kurodie-eng <leongamer239@gmail.com>
2026-06-19 22:59:51 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -9,7 +9,7 @@ date_default_timezone_set('America/Mexico_City');
 
 // Conexión a la BD SQL de pruebas,
                     //  Server     Usuario  Contraseña     NombreBD
-$conexion=new mysqli("localhost",  "root",   "12345",   "solicitudes");
+$conexion=new mysqli("mysql.railway.internal","root","ybrkvaWZMdsexhYnUHVRcjauVXZWgdKg","solicitudes");
 $conexion->set_charset("utf8");
 
 ?>
