a83f74a5f1bc655b063c9c43eae133d1a5e7e51a
José Uriel Villanueva Donjuan <j.uriel.vilanueva.d@gmail.com>
2026-06-19 22:20:23 -0600

    Hola

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión — ITSRV Soporte Técnico</title>
+    <title>Iniciar Sesión — ITSRV Soporte Técnico S.L.P</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="pagina-login">
